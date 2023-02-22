6934b0a1dba613ca26c02fc25ac923b18288bb62
Maciej Krzysztoforski <krzysztoforskimaciek@gmail.com>
2023-02-23 00:03:51 +0100

    feat: some styling

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -24,24 +24,26 @@ const Login = () => {
   };
 
   return (
-    <div className='w-full h-[100vh] flex flex-col justify-center items-center'>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <div className='w-full h-[100vh] flex flex-row p-8 bg-[#fafafa]'>
+      <div className='w-[40%] h-[calc(100vh-32px)] flex flex-col justify-center items-center'>
+      <h1 className='mb-4 text-[24px]'>Sign in to your account</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col items-start gap-4 w-[350px]'>
       <div>
         <label>
-          Username:
-          <input type="text" value={username} onChange={handleUsernameChange} />
+          <input className='border-[#e8e8e8] border-b-[1px] rounded p-1 h-[40px] w-[350px] text-sm pl-3' type="text" placeholder='Login' value={username} onChange={handleUsernameChange} />
         </label>
       </div>
       <div>
         <label>
-          Password:
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <input className='border-[#e8e8e8] border-b-[1px] rounded p-1 h-[40px] w-[350px] text-sm pl-3' type="password" placeholder='Password' value={password} onChange={handlePasswordChange} />
         </label>
       </div>
-      <button type="submit">Log In</button>
+      <button className='w-full h-[45px] bg-black text-white rounded hover:scale-105 transition delay-150 duration-300 ease-in-out' type="submit">Sign in</button>
       </form>
-      <button onClick={handleClick}>No account? Click here.</button>
+      <button className='mt-8 text-sm text-[#7f7f88]' onClick={handleClick}>No account? <span className='underline'>Click here.</span></button>
+    </div>
+      <div className='w-[60%] h-[calc(100vh-64px)] bg-slate-500 rounded-tl-[60px] rounded-br-[60px]'>
+      </div>
     </div>
   );
 }
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -35,36 +35,36 @@ const Register = () => {
   };
 
   return (
-    <div className='w-full h-[100vh] flex flex-col justify-center items-center'>
-      <h1>Create an account</h1>
-      <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={handleEmailChange} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Confirm Password:
-          <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
-        </label>
-      </div>
-      <button type="submit">Register</button>
+    <div className='w-full h-[100vh] flex flex-row p-8 bg-[#fafafa]'>
+      <div className='w-[40%] h-[calc(100vh-32px)] flex flex-col justify-center items-center'>
+      <h1 className='mb-4 text-[24px]'>Create an account</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col items-start gap-4 w-[350px]'>
+        <div>
+          <label className='w-full'>
+            <input className='border-[#e8e8e8] border-b-[1px] rounded p-1 h-[40px] w-[350px] text-sm pl-3' type="text" placeholder='Username' value={username} onChange={handleUsernameChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input className='border-[#e8e8e8] border-b-[1px] rounded p-1 h-[40px] w-[350px] text-sm pl-3' type="email" placeholder='Email' value={email} onChange={handleEmailChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input className='border-[#e8e8e8] border-b-[1px] rounded p-1 h-[40px] w-[350px] text-sm pl-3' type="password" placeholder='Password' value={password} onChange={handlePasswordChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input className='border-[#e8e8e8] border-b-[1px] rounded p-1 h-[40px] w-[350px] text-sm pl-3' type="password" placeholder='Password' value={confirmPassword} onChange={handleConfirmPasswordChange} />
+          </label>
+        </div>
+        <button className='w-full h-[45px] bg-black text-white rounded hover:scale-105 transition delay-150 duration-300 ease-in-out' type="submit">Sign up</button>
       </form>
-      <button onClick={handleClick}>Already registered? Click here.</button>
+      <button className='mt-8 text-sm text-[#7f7f88]' onClick={handleClick}>Already registered? <span className='underline'>Click here.</span></button>
+    </div>
+      <div className='w-[60%] h-[calc(100vh-64px)] bg-slate-500 rounded-tl-[60px] rounded-br-[60px]'>
+      </div>
     </div>
   );
 }
